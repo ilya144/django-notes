7043e8acd20de476df7663673cba1065c589f765
ilya144 <ils12rus@gmail.com>
2019-10-26 11:57:31 +0300

    add template and static setting

--- django-notes/settings.py
+++ django-notes/settings.py
@@ -56,7 +56,11 @@ ROOT_URLCONF = 'django-notes.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            "".join([
+                BASE_DIR, "/webclient/templates"
+            ])
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,3 +124,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+     os.path.join(BASE_DIR, "webclient/static"),
+]
