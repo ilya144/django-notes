23a77766adb4a076d0b7fcca3a34c923700b586e
ilya144 <ils12rus@gmail.com>
2020-02-16 14:41:09 +0300

    add auth api

--- notes/urls.py
+++ notes/urls.py
@@ -5,8 +5,12 @@ from . import views
 router = routers.DefaultRouter()
 router.register(r'notes', views.NoteViewSet)
 router.register(r'categories', views.CategoryViewSet)
+router.register(r'users', views.UserViewSet)
 
 urlpatterns = [
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    path('register', views.register_view),
+    path('login', views.login_view),
+    path('logout', views.logout_view),
     # path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
--- notes/views.py
+++ notes/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from .serializers import NoteSerializer, CategorySerializer
+from .serializers import NoteSerializer, CategorySerializer, UserSerializer
 from .models import Note, Category
+from django.contrib.auth.models import User
+from django.http import HttpResponseBadRequest, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
 
-# Create your views here.
+# API View Sets
 class NoteViewSet(viewsets.ModelViewSet):
     queryset = Note.objects.all().order_by('-date')
     serializer_class = NoteSerializer
@@ -11,3 +15,40 @@ class NoteViewSet(viewsets.ModelViewSet):
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+# API User Session control
+
+@csrf_exempt
+def register_view(request):
+    if request.method != 'POST':
+        return HttpResponseBadRequest()
+    
+    serializer = UserSerializer( data=json.loads(request.body) )
+    print(serializer.is_valid())
+    # print(serializer.validated_data)
+    if serializer.is_valid():
+        print(serializer.create(serializer.validated_data))
+    else:
+        print(serializer._errors)
+
+    return JsonResponse({"endpoint": "reg", "status": "ok"})
+
+@csrf_exempt
+def login_view(request):
+    if request.method != 'POST':
+        request.session["name"] = "test"
+        return HttpResponseBadRequest()
+
+    return JsonResponse({"endpoint": "login", "status": "ok"})
+
+@csrf_exempt
+def logout_view(request):
+    if request.method != 'POST':
+        request.session.flush()
+        return HttpResponseBadRequest()
+
+    return JsonResponse({"endpoint": "logout", "status": "ok"})
